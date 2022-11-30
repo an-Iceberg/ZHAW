786a1bec1af4044e8e7782c8ccf9e83b4ac628ba
an-Iceberg <a.g.marosi@bluewin.ch>
2022-11-30 19:06:36 +0100

    Adjusted the "AudioTest.java" File (mostly just whitespace)

--- HS_2022/information_und_codierung/praktikum_9/AudioTest.java
+++ HS_2022/information_und_codierung/praktikum_9/AudioTest.java
@@ -19,93 +19,93 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class AudioTest 
+public class AudioTest
 {
-    static RandomAccessFile raw;
-    static int byteCount = 0;
+  static RandomAccessFile raw;
+  static int byteCount = 0;
 
-    public static void main(String[] args)
+  public static void main(String[] args)
+  {
+    int sampleRate = 44100;                                                         // Sampling Rate
+    int bitsPerSample = 16;                                                         // Bits per Sample
+    int nChannels = 1;                                                              // Number of Channels
+    int duration = 3;                                                               // Duration in seconds
+
+    try
     {
-        int sampleRate = 44100;                                                         // Sampling Rate
-        int bitsPerSample = 16;                                                         // Bits per Sample
-        int nChannels = 1;                                                              // Number of Channels
-        int duration = 3;                                                               // Duration in seconds 
+      raw = new RandomAccessFile("AudioFile.wav", "rw");
+      raw.setLength(0); // Set file length to 0, to prevent unexpected behavior in case the file already existed
 
-        try
-        {
-            raw = new RandomAccessFile("AudioFile.wav", "rw");
-            raw.setLength(0); // Set file length to 0, to prevent unexpected behavior in case the file already existed
+      raw.writeBytes("RIFF");
+      raw.writeInt(0); // Final file size not known yet, write 0. This is = sample count + 36 bytes from header.
+      raw.writeBytes("WAVE");                                                     // Keyword "WAVE"
+      raw.writeBytes("fmt ");                                                     // Keyword "fmt "
+      raw.writeInt(Integer.reverseBytes(16));                                     // Sub-chunk size, 16 for PCM
+      raw.writeShort(Short.reverseBytes((short) 1));                              // AudioFormat, 1 for PCM
 
-            raw.writeBytes("RIFF");
-            raw.writeInt(0); // Final file size not known yet, write 0. This is = sample count + 36 bytes from header.
-            raw.writeBytes("WAVE");                                                     // Keyword "WAVE"
-            raw.writeBytes("fmt ");                                                     // Keyword "fmt "
-            raw.writeInt(Integer.reverseBytes(16));                                     // Sub-chunk size, 16 for PCM
-            raw.writeShort(Short.reverseBytes((short) 1));                              // AudioFormat, 1 for PCM
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // TODO: Hier fehlende Header Eintraege ergaenzen
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // TODO: Hier fehlende Header Eintraege ergaenzen
-            
-                                                                                        // Number of channels, 1 for mono, 2 for stereo
-                                                                                        // Sample rate
-                                                                                        // Byte rate 
-                                                                                        // Block align, nChannels*bitsPerSample/8
-                                                                                        // Bits per Sample 
-                                                                                        // Keyword "data"
+                                                                                  // Number of channels, 1 for mono, 2 for stereo
+                                                                                  // Sample rate
+                                                                                  // Byte rate
+                                                                                  // Block align, nChannels*bitsPerSample/8
+                                                                                  // Bits per Sample
+                                                                                  // Keyword "data"
 
-                                                                                        
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            raw.writeInt(0);                                                            // Data chunk size not known yet, write 0
-        }
-        catch(IOException e)
-        {
-            System.out.println("I/O exception occured while writing data");
-        }
 
-        // Audio Samples generieren
-        for (int i = 0; i < sampleRate*duration; i++)
-        {
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // TODO: Hier Audio Samples erzeugen
-            
-            
-            writeSample(.....);
-            
-            
-            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        }
-
-        closeFile();
-        System.out.println("Finished");
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      raw.writeInt(0);                                                            // Data chunk size not known yet, write 0
+    }
+    catch(IOException e)
+    {
+      System.out.println("I/O exception occured while writing data");
     }
 
-    static void writeSample(double doubleValue)
+    // Audio Samples generieren
+    for (int i = 0; i < sampleRate*duration; i++)
     {
-        try 
-        {
-            short shortSample = (short)(doubleValue);
-            raw.writeShort(Short.reverseBytes(shortSample));
-            byteCount += 2;
-        }
-        catch(IOException e)
-        {
-            System.out.println("I/O exception occured while writing data");
-        }
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // TODO: Hier Audio Samples erzeugen
+
+
+      writeSample(.....);
+
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
 
-    static void closeFile() 
+    closeFile();
+    System.out.println("Finished");
+  }
+
+  static void writeSample(double doubleValue)
+  {
+    try
     {
-        try 
-        {
-            raw.seek(4); 
-            raw.writeInt(Integer.reverseBytes(byteCount + 36));
-            raw.seek(40); 
-            raw.writeInt(Integer.reverseBytes(byteCount));
-            raw.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println("I/O exception occured while closing output file");
-        }
+      short shortSample = (short)(doubleValue);
+      raw.writeShort(Short.reverseBytes(shortSample));
+      byteCount += 2;
     }
+    catch(IOException e)
+    {
+      System.out.println("I/O exception occured while writing data");
+    }
+  }
+
+  static void closeFile()
+  {
+    try
+    {
+      raw.seek(4);
+      raw.writeInt(Integer.reverseBytes(byteCount + 36));
+      raw.seek(40);
+      raw.writeInt(Integer.reverseBytes(byteCount));
+      raw.close();
+    }
+    catch(IOException e)
+    {
+      System.out.println("I/O exception occured while closing output file");
+    }
+  }
 }

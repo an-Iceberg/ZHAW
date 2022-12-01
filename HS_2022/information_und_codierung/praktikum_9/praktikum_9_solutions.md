916c1ef6c346afc0036de0589d802242ea32450b
an-Iceberg <a.g.marosi@bluewin.ch>
2022-12-01 21:03:10 +0100

    Created second audio generation project; solved some additional exercises

--- HS_2022/information_und_codierung/praktikum_9/praktikum_9_solutions.md
+++ HS_2022/information_und_codierung/praktikum_9/praktikum_9_solutions.md
@@ -6,7 +6,7 @@
 
 # 2.
 
-$$S_i = K \cdot \sin\left({i \cdot 2 \pi f \over R}\right)$$
+>$$S_i = K \cdot \sin\left({i \cdot 2 \pi f \over R}\right)$$
 
 $$K = 2^{\text{bit per sample} - 1} - 1$$
 
@@ -16,7 +16,16 @@ $$\boxed{K = 32'767}$$
 
 # 4.
 
+$\text{Anzahl Stützpunkte} = 45$
+
+$\text{Dynamik} = 2$
+
+$\text{Sampling Rate} \div \text{Frequenz} = 44'100 \div 1'000 \simeq 45$
 # 5.
+
+Es hatte bei mir keine fehlerhafte Darstellung des Audiosignals gegeben.
+
+Die Überlagerung von 2 Wellen
 
 # 6.
 
---
+++ HS_2022/information_und_codierung/praktikum_9/signal1/src/main.rs
@@ -0,0 +1,63 @@
+use std::{fs, io::{Write, Seek}, f32::consts::PI};
+
+fn main()
+{
+  let mut file;
+
+  match fs::File::create("file.wav")
+  {
+    Ok(file_handle) =>
+      file = file_handle,
+
+    Err(err) =>
+    {
+      println!("{err}");
+      std::process::exit(-1);
+    }
+  };
+
+  let sample_rate: u32 = 44_100;
+  let bits_per_sample: u16 = 16;
+  let number_of_channels: u16 = 1;
+  let duration: u32 = 3;
+  let mut byte_count: u32 = 1;
+
+  file.write(b"RIFF").unwrap();
+  file.write(&2_084_u32.to_le_bytes()).unwrap(); // Final file size not known yet, write 0. This is = sample count + 36 bytes from header.
+  file.write(b"WAVE").unwrap(); // Keyword "WAVE"
+  file.write(b"fmt ").unwrap(); // Keyword "fmt "
+  file.write(&16_u32.to_le_bytes()).unwrap(); // Sub-chunk size, 16 for PCM (2 bytes)
+  file.write(&1_u16.to_le_bytes()).unwrap(); // AudioFormat, 1 for PCM (2 bytes)
+
+  file.write(&number_of_channels.to_le_bytes()).unwrap(); // Number of channels, 1 for mono, 2 for stereo
+  file.write(&sample_rate.to_le_bytes()).unwrap(); // Sample rate
+  file.write(&((sample_rate * bits_per_sample as u32 * number_of_channels as u32) / 8).to_le_bytes()).unwrap(); // Byte rate
+  file.write(&((bits_per_sample * number_of_channels as u16) / 8).to_le_bytes()).unwrap(); // Block align
+  file.write(&bits_per_sample.to_le_bytes()).unwrap(); // Bits per Sample
+  file.write(b"data").unwrap(); // Keyword "data"
+
+  file.write(&0_u32.to_le_bytes()).unwrap(); // Data chunk size not known yet, write 0
+
+  // let frequency: f32 = 2_f32 * sample_rate as f32;
+  let frequency = 1_000_f32;
+  // $K = 2^{\text{bits per sample} - 1} - 1$
+  let scaling_factor_k: f32 = 2_f32.powf(bits_per_sample as f32 - 1_f32) - 1_f32;
+
+  for i in 0..(sample_rate * duration)
+  {
+    // TODO: make some noise!!
+    // $S_i = K \cdot \sin\left({i \cdot 2\pi \cdot f \over R}\right)$
+    let sample = (scaling_factor_k * ((i as f32 * 2_f32 * PI * frequency) / sample_rate as f32).sin()) as i16;
+
+    file.write(&sample.to_le_bytes()).unwrap();
+    byte_count += 2;
+  }
+
+  println!("\nbyte_count:{}\n", byte_count);
+
+  file.seek(std::io::SeekFrom::Start(0x_4)).unwrap();
+  file.write(&(byte_count + 36).to_le_bytes()).unwrap();
+
+  file.seek(std::io::SeekFrom::Start(0x_28)).unwrap();
+  file.write(&byte_count.to_le_bytes()).unwrap();
+}
